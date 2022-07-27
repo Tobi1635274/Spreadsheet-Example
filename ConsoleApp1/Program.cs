da1b58788af4fb7cf3d8383e6a44a02bb919db97
Tobi1635274 <78792973+Tobi1635274@users.noreply.github.com>
2022-07-27 10:26:27 +0200

    Console App for SpreadsheetLite example

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using SpreadsheetLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         using (SLDocument sl = new SLDocument($"d:\\tmp\\ipsYdion ZV Abrechnung Master - ipsYdion - alt.xlsx", "Tabelle1"))
+         {
+            //var templateSheet = new SLDocument($"d:\\tmp\\IpsYdion ZV Abrechnung Master.xlsx", "Tabelle1");
+            sl.SaveAs($"d:\\tmp\\abc.xlsx");
+         }
+      }
+   }
+}
